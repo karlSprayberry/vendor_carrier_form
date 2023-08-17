766451f16294f5b83adbe61f039959a7e443f95d
Barrett Purohit <barrett.purohit@Rheem.com>
2023-08-17 16:14:52 -0500

    Added fields except Privacy/Security to API must test

--- main.py
+++ main.py
@@ -137,6 +137,84 @@ def create():
                                    {
                                        "fieldName": "DO_YOU_PLAN_TO_USE_ANY_THIRD_PARTIES_OTHER_THAN_TRANSPORT_SERVICES_IN_PROVIDING_GOODS_SERVICES_P",
                                        "Values": [request.form['thirdPartiesRadio']]
+                                   },
+                                   
+
+                                   {
+                                       "fieldName": "ARE_THERE_ANY_CIVIL_CRIMINAL_OR_ADMINISTRATIVE_JUDGMENTS_CLAIMS_SUITS_OR_ARBITRATION_PROCEEDINGS_PENDING_OR_OUTSTANDING_AGAINST_YOUR_BUSINESS_OR_ITS_OFFICERS_P",
+                                       "Values": [request.form['outstandingJudgementsRadio']]
+                                   },
+                                   {
+                                       "fieldName": "HAS_A_CONSENT_DECREE_EVER_BEEN_ISSUED_AGAINST_YOUR_COMPANY_OFFICERS_OR_DIRECTORS_BY_ANY_GOVERNMENT_ENTITY_P",
+                                       "Values": [request.form['consentDecreeRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IS_YOUR_COMPANY_UNDER_AUDIT_OR_INVESTIGATION_IN_CONNECTION_WITH_THE_GOODS_OR_SERVICES_THAT_YOU_PROVIDE_TO_RHEEM_P",
+                                       "Values": [request.form['auditRadio']]
+                                   },
+                                   
+
+                                   {
+                                       "fieldName": "NON_U_S_PUBLIC_OFFICIAL_MAY_INCLUDE_EMPLOYEES_OF_GOVERNMENT_AGENCIES_OWNED_BUSINESSES_SUCH_AS_STATE_OWNED_ENTERPRISES_AND_A_POLITICAL_PARTY_OR_POLITICAL_CANDIDATE_P",
+                                       "Values": [request.form['governmentEmployeeRadio']]
+                                   },
+                                   {
+                                       "fieldName": "DOES_ANY_KEY_EMPLOYEE_OR_SENIOR_MANAGEMENT_MEMBER_OF_THE_BUSINESS_PROVIDE_FINANCIAL_OR_ANY_OTHER_BENEFITS_TO_A_NON_U_S_PUBLIC_OFFICIAL_OR_A_MEMBER_OF_A_NON_U_S_PUBLIC_OFFICIAL_S_FAMILY_P",
+                                       "Values": [request.form['publicOfficialRadio']]
+                                   },
+                                   {
+                                       "fieldName": "DOES_ANY_NON_U_S_PUBLIC_OFFICIAL_OR_A_MEMBER_OF_A_NON_U_S_PUBLIC_OFFICIAL_S_FAMILY_STAND_TO_BENEFIT_IN_ANY_WAY_AS_A_RESULT_OF_THE_PROPOSED_AGREEMENT_P",
+                                       "Values": [request.form['nonUSFamiliyBenefitRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IS_ANY_KEY_EMPLOYEE_OR_SENIOR_MANAGEMENT_OF_THE_BUSINESS_RELATED_BY_BLOOD_MARRIAGE_CURRENT_BUSINESS_ASSOCIATION_OR_OTHERWISE_TO_A_NON_U_S_PUBLIC_OFFICIAL_P",
+                                       "Values": [request.form['nonUSRelationRadio']]
+                                   },
+                                   
+
+
+                                   {
+                                       "fieldName": "HAS_ANY_KEY_EMPLOYEE_OR_SENIOR_OFFICER_OF_YOUR_BUSINESS_EVER_BEEN_EMPLOYED_BY_OR_PERFORMED_SERVICES_FOR_RHEEM_P",
+                                       "Values": [request.form['employeeRheemRadio']]
+                                   },
+                                   {
+                                       "fieldName": "ARE_ANY_OF_YOUR_BUSINESS_PRINCIPALS_RELATED_TO_ANYONE_AT_RHEEM_P",
+                                       "Values": [request.form['companyRelationRadio']]
+                                   },
+                                   
+
+
+                                   {
+                                       "fieldName": "DOES_THE_COMPANY_HAVE_COMMERCIAL_GENERAL_LIABILITY_COVERAGE_P",
+                                       "Values": [request.form['liabilityCoverageRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IF_YES_COVERAGE_LIMITS_P",
+                                       "Values": [request.form['coverageLimitLiability']]
+                                   },
+                                   {
+                                       "fieldName": "DOES_THE_COMPANY_HAVE_WORKERS_COMPENSATION_COVERAGE_P",
+                                       "Values": [request.form['workersCompensationRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IF_YES_COVERAGE_LIMITS_1_P",
+                                       "Values": [request.form['coverageLimitWorkerComp']]
+                                   },
+                                   {
+                                       "fieldName": "INDICATE_RETENTION_AMOUNT_P",
+                                       "Values": [request.form['retentionAmount']]
+                                   },
+                                   {
+                                       "fieldName": "IF_YOU_PROVIDE_PRODUCTS_OR_PERFORM_ANY_WORK_FROM_RHEEM_DOES_THE_COMPANY_HAVE_PRODUCT_LIABILITY_COMPLETED_OPERATIONS_INSURANCE_COVERAGE_P",
+                                       "Values": [request.form['operationsInsuranceRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IF_YOU_PROVIDE_SERVICES_TO_RHEEM_DOES_THE_COMPANY_HAVE_ERRORS_AND_OMISSION_LIABILITY_COVERAGE_P",
+                                       "Values": [request.form['omissionLiabilityRadio']]
+                                   },
+                                   {
+                                       "fieldName": "IF_YES_COVERAGE_LIMITS_3_P",
+                                       "Values": [request.form['coverageOmissionLiability']]
                                    }
                      
                                ]
